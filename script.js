462f6c228dab13d77850f74c9422e4dd8a65d6c8
vudinhson2001 <you@example.com>
2022-03-25 17:23:16 +0700

    add case

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+//JS cho thanh Menu
 const menubar = document.querySelector(".menu-bar")
 menubar.addEventListener("click", function () {
     menubar.classList.toggle("active")
-document.querySelector(".menu-items").classList.toggle("active")
+    document.querySelector(".menu-items").classList.toggle("active")
 })
-const Top= document.querySelector(".top")
-window.addEventListener("scroll",function (){
+const Top = document.querySelector(".top")
+window.addEventListener("scroll", function () {
     const x = this.pageYOffset;
-    if(x>80){Top.classList.add("active")}
-    else{Top.classList.remove("active")}
+    if (x > 80) {
+        Top.classList.add("active")
+    } else {
+        Top.classList.remove("active")
+    }
 
- })
+})
+//JS cho menu
+const menuTitle = document.querySelector(".menu-title");
+menuTitle.addEventListener("click", function (x) {
+    if (x.target.classList.contains("menu-button")) {
+        const Target = x.target.getAttribute("data-title");
+        menuTitle.querySelector(".active").classList.remove("active");
+        x.target.classList.add("active");
+        const dogItem = document.querySelector(".menu");
+        dogItem.querySelector(".menu-item-content.active").classList.remove("active");
+        dogItem.querySelector(Target).classList.add("active")
+    }
+})
+
